bd13e2c0a01a37d6b59a1722013d4ee8dc1b5c8d
GriffithBeliever <dopeanation@hotmail.com>
2018-01-31 14:56:42 +0200

    added an updated ver of doing the same thing

--- mutex.go
+++ mutex.go
@@ -8,61 +8,121 @@
 package ddbsync
 
 import (
-	"fmt"
-	"time"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
+	session2 "github.com/aws/aws-sdk-go/aws/session"
+	"github.com/Towbe/TowbeApi/utils/helpers/env"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
 )
+
+type MutexKey struct {
+	Id string `json:"id"`
+}
 
 // A Mutex is a mutual exclusion lock.
 // Mutexes can be created as part of other structures.
 type Mutex struct {
-	Name string
+	MutexKey
 	Ttl  int64
+}
+
+var session *session2.Session
+
+func NewMutex() *Mutex {
+	var config *aws.Config
+
+	if env.IsAWS() {
+		config = aws.NewConfig().WithRegion("us-east-1")
+	} else {
+		creds := credentials.NewSharedCredentials("", "towbe-dev")
+		config = aws.NewConfig().WithRegion("us-east-1").WithCredentials(creds)
+	}
+	if db == nil {
+		if session == nil {
+			session = session2.New(config)
+		}
+		db = dynamodb.New(session)
+	}
+
+	return &Mutex{}
 }
 
 // Lock will write an item in a DynamoDB table if the item does not exist.
 // Before writing the lock, we will clear any locks that are expired.
 // Calling this function will block until a lock can be acquired.
-func (m *Mutex) Lock() {
+func (m *Mutex) Lock() error {
 	for {
 		m.PruneExpired()
-		err := db.put(m.Name, time.Now().Unix())
+		putItemInput := &dynamodb.PutItemInput{
+			TableName:	aws.String("towbe_locks"),
+		}
+		marshalledMutex, err := dynamodbattribute.MarshalMap(m)
+		if err != nil {
+			return err
+		}
+		putItemInput.SetConditionExpression("attribute_not_exists(hash)")
+		putItemInput.SetItem(marshalledMutex)
+		_, err = db.PutItem(putItemInput)
 		if err == nil {
-			return
+			return nil
 		}
 	}
 }
 
 func (m *Mutex) LockOrFail() error {
 	m.PruneExpired()
-	err := db.put(m.Name, time.Now().Unix())
+	putItemInput := &dynamodb.PutItemInput{
+		TableName:	aws.String("towbe_locks"),
+	}
 
+	marshalledMutex, err := dynamodbattribute.MarshalMap(m)
+	if err != nil {
+		return err
+	}
+	putItemInput.SetConditionExpression("attribute_not_exists(id)")
+	putItemInput.SetItem(marshalledMutex)
+	_, err = db.PutItem(putItemInput)
 	return err
 }
 
 // Unlock will delete an item in a DynamoDB table.
-func (m *Mutex) Unlock() {
+func (m *Mutex) Unlock() error {
 	for {
-		err := db.delete(m.Name)
+		deleteItemInput := &dynamodb.DeleteItemInput{
+			TableName:	aws.String("towbe_locks"),
+		}
+		//deleteItemInput.SetConditionExpression("attributeExists(name)")
+		marshalledKey, err := dynamodbattribute.MarshalMap(m.MutexKey)
+		if err != nil {
+			return err
+		}
+
+		deleteItemInput.SetKey(marshalledKey)
+		_, err = db.DeleteItem(deleteItemInput)
 		if err == nil {
-			return
+			return nil
 		}
 	}
 }
+
+
 
 // PruneExpired delete all locks that have lived past their TTL.
 // This is to prevent deadlock from processes that have taken locks
 // but never removed them after execution. This commonly happens when a
 // processor experiences network failure.
-func (m *Mutex) PruneExpired() {
-	item, err := db.get(m.Name)
+func (m *Mutex) PruneExpired() error {
+	getItemInput := &dynamodb.GetItemInput{
+		TableName:	aws.String("towbe_locks"),
+	}
+
+	marshalledKey, err := dynamodbattribute.MarshalMap(m.MutexKey)
 	if err != nil {
-		fmt.Printf("error=%v", err)
-		return
+		return err
 	}
-	if item != nil {
-		if item.Created < (time.Now().Unix() - m.Ttl) {
-			m.Unlock()
-		}
-	}
-	return
+
+	getItemInput.SetKey(marshalledKey)
+	_, err = db.GetItem(getItemInput)
+	return err
 }
